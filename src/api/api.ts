f117e895899116e5d47f2989aa274b441736ec51
FE_LeeJungHwan <antglljung@naver.com>
2023-08-01 17:50:52 +0900

    docs: 연차컴포넌트 api분류

--- src/api/api.ts
+++ src/api/api.ts
@@ -35,3 +35,29 @@ export const userApproveApi = async() => {
     throw error
   }
 }
+
+export const vacationApi = async() => {
+  try {
+    const data = await axios.get(`${API_URL}/api/admin/vacation/approve/list`)
+    if(data) {
+      return data
+    } else {
+      throw new Error('기록 작성에 실패하셨습니다')
+    } 
+  } catch(error) {
+    throw(error)
+  }
+}
+
+export const vacationPendingApi = async() => {
+  try {
+    const data = await axios.get(`${API_URL}/api/admin/vacation/pending`)
+    if(data) {
+      return data
+    } else {
+      throw new Error('기록 작성에 실패하셨습니다')
+    } 
+  } catch(error) {
+    throw(error)
+  }
+}
